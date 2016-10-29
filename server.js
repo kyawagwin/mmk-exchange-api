3ce1186edd733c622c32c175707ee9ba80af0d32
Kyaw Aung Win <kyawagwin@gmail.com>
2016-10-29 11:28:34 +0800

    inverse rate for history

--- server.js
+++ server.js
@@ -198,7 +198,8 @@ function saveRate(queryDate, callback) {
 
             for (var property in rateJson.rates) {
                 if (typeof rate.get(property) != 'undefined') {
-                    rate.set(property, rateJson.rates[property]);
+                  // TODO: temporary change inverse rate for myanmar currency
+                    rate.set(property, 1 / rateJson.rates[property]);
                 }
             }
 
