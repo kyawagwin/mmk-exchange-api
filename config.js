9caf1278a7a57433a8c9c66a261ea42465bfb3e7
Kyaw Aung Win <kyawagwin@gmail.com>
2017-01-03 19:39:22 +0800

    Update config.js
    credential change for mongo lab

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     secret: '4youmyl0ve',
-    database: 'mongodb://admin:welcome123@ds031975.mlab.com:31975/mmk-exchange',
+    database: 'mongodb://mmkexchange:32592135@ds031975.mlab.com:31975/mmk-exchange',
     apiKey: '5b8f552747da4235907d8e5ec87b1655'
 }
